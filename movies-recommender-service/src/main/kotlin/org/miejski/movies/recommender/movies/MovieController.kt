63dff8d85a4f6c3f2862c207d1a3ce69a494ed4d
gmiejski <grz.miejski@gmail.com>
2016-03-20 19:17:13 +0100

    MR-4 | Moved service into dedicated module.

--- movies-recommender-service/src/main/kotlin/org/miejski/movies/recommender/movies/MovieController.kt
+++ movies-recommender-service/src/main/kotlin/org/miejski/movies/recommender/movies/MovieController.kt
@@ -1,7 +1,6 @@
 package org.miejski.movies.recommender.movies
 
 import org.miejski.movies.recommender.domain.Movie
-import org.miejski.movies.recommender.domain.Person
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
