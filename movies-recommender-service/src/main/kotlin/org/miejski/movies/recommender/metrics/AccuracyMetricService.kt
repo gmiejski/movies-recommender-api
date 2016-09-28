3646e1a5a1d6b1d3c071e39989fa390e16d1c0f8
gmiejski <grz.miejski@gmail.com>
2016-09-28 16:55:51 +0200

    MR-18 | Speed up acc metric

--- movies-recommender-service/src/main/kotlin/org/miejski/movies/recommender/metrics/AccuracyMetricService.kt
+++ movies-recommender-service/src/main/kotlin/org/miejski/movies/recommender/metrics/AccuracyMetricService.kt
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service
 import java.lang.System.currentTimeMillis
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 @Service
 open class AccuracyMetricService @Autowired constructor(val recommendationsService: RecommendationsService,
@@ -17,18 +19,26 @@ open class AccuracyMetricService @Autowired constructor(val recommendationsServi
     fun run(testFilePath: String?): Double {
         val allLines = Files.readAllLines(Paths.get(testFilePath))
         logger.warn("Looking for predictions for {} ratings.", allLines.size.toString())
-
+        val newFixedThreadPool = Executors.newFixedThreadPool(15)
         val start = currentTimeMillis()
 
-        val predictedRatings = allLines.drop(1)
+        val tasks = allLines.drop(1)
             .map { toRating(it) }
-            .map { joinWithPrediction(it) }
+            .map {
+                Callable<Pair<Double, Double>>({
+                    Pair(it.rating, recommendationsService.predictedRating(it.person, it.movie))
+                })
+            }
+
+        val predictions = newFixedThreadPool.invokeAll(tasks)
+        newFixedThreadPool.shutdown()
+
+        val predictedRatings = predictions.map { it.get() }
             .filter { it.second > 0 }
-        logger.info("Found rating for {} movies", predictedRatings.size)
 
         val result = RMSEMetric.calculate(predictedRatings)
         val stop = currentTimeMillis()
-
+        logger.warn("Found rating for {} movies", predictedRatings.size)
         logger.warn("Resulting rmse = {} from fold: {} in time: {} seconds", result.toString(), testFilePath, (stop - start) / 1000.0)
         return result
     }
