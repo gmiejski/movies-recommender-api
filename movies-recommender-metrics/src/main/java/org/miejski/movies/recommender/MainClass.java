cd80eb34da095e1bb8ce96a75903e17fd9ce99bd
gmiejski <grz.miejski@gmail.com>
2017-04-05 22:46:39 +0200

    MR-20 | run metrics from command line.

--- movies-recommender-metrics/src/main/java/org/miejski/movies/recommender/MainClass.java
+++ movies-recommender-metrics/src/main/java/org/miejski/movies/recommender/MainClass.java
@@ -1,13 +1,21 @@
 package org.miejski.movies.recommender;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
         System.out.println("START");
         new CypherExecutor().execute();
-//        System.out.println(Arrays.toString(args));
-//        Files.readAllLines(Paths.get(args[0]));
+        if (args.length > 0) {
+            System.out.println(Arrays.toString(args));
+            Stream<Path> list = Files.list(Paths.get(args[0]));
+            list.forEach(System.out::println);
+        }
 
         System.out.println("END");
     }
