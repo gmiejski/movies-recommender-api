a6f2fd50034104044657b59babd9eccc10eaedcf
gmiejski <grz.miejski@gmail.com>
2017-05-31 22:48:42 +0200

    MR-24 | Plotting real CPU results.

--- deployment/scripts/performance_tests_main.py
+++ deployment/scripts/performance_tests_main.py
@@ -10,12 +10,12 @@ config = {
         "instance-type": "t2.micro"
     },
     "service": {
-        "count": 1,
-        "instance-type": "t2.micro"
+        "count": 0,
+        "instance-type": "t2.small"
     },
     "test-driver": {
-        "count": 1,
-        "instance-type": "t2.micro"
+        "count": 0,
+        "instance-type": "t2.small"
     }
 }
 
@@ -23,29 +23,41 @@ instance_configurer = InstanceConfigurer()
 instance_configurer.load_existing_instances()
 instance_configurer.prepare_instances(config)
 instance_configurer.wait_for_instances()
-instance_configurer.run_apps(dryRun=True)
-# instance_configurer.run_apps(dryRun=False)
+# instance_configurer.run_apps(dryRun=True)
+instance_configurer.run_apps(dryRun=False)
 
 service_checker = InstanceStateChecker(instance_configurer.service_ips())
 service_checker.wait_for_services()
 
 warmup_config = {
-    "max_users": 1,
-    "wait_interval": 100,
-    "run_time": 1
+    "max_users": 50,
+    "wait_interval": 500,
+    "run_time": 2
 }
 
-AnsibleRunner.run_warmup_on_driver(instance_configurer.test_driver_ip(), "RatingsSimulation", warmup_config)
+# AnsibleRunner.run_warmup_on_driver(instance_configurer.test_driver_ip(), "RatingsSimulation", warmup_config)
+# AnsibleRunner.run_warmup_on_driver(instance_configurer.test_driver_ip(), "RecommendationsSimulation", warmup_config)
+
 
 AnsibleRunner.start_collecting_metrics(instance_configurer.neo4jInstances.ips())
 
 
-simulation_config = {
-    "max_users": 1,
-    "wait_interval": 100,
+reco_config = {
+    "max_users": 30,
+    "wait_interval": 500,
     "run_time": 1
 }
 
-# AnsibleRunner.run_tests_on_driver(instance_configurer.test_driver_ip(), "RecommendationsSimulation", simulation_config)
-AnsibleRunner.run_tests_on_driver(instance_configurer.test_driver_ip(), "RatingsSimulation", simulation_config)
+ratings_config = {
+    "max_users": 150,
+    "wait_interval": 50,
+    "run_time": 1
+}
+
+AnsibleRunner.run_tests_on_driver(instance_configurer.test_driver_ip(), "RecommendationsSimulation", reco_config)
+# AnsibleRunner.run_tests_on_driver(instance_configurer.test_driver_ip(), "RatingsSimulation", ratings_config)
+
 AnsibleRunner.download_os_metrics(instance_configurer.neo4jInstances.instances[0].publicIp)
+
+print("Reco -> Rq/s : {}".format(1000 / reco_config['wait_interval'] * reco_config['max_users']))
+print("Ratings -> Rq/s : {}".format(1000 / ratings_config['wait_interval'] * ratings_config['max_users']))
